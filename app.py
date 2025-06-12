42c43daf44441fcb2a97068474b9cb153e3c7291
Cesar Blanco <cesarablanco5@gmail.com>
2025-06-12 01:11:00 -0400

    Subiendo proyecto con archivos HTML corregidos y CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template_string, send_file
+from flask import Flask, request, redirect, url_for, render_template, send_file
 import qrcode
 import sqlite3
 import uuid
@@ -15,62 +15,6 @@ c.execute('''CREATE TABLE IF NOT EXISTS personas
              (id TEXT PRIMARY KEY, nombres TEXT, apellidos TEXT, cedula TEXT, fecha_emision TEXT, cargo TEXT)''')
 conn.commit()
 conn.close()
-
-# HTML básico
-form_html = '''
-<!doctype html>
-<title>Agregar Persona</title>
-<h2>Agregar Persona</h2>
-<form method=post>
-  Nombres: <input type=text name=nombres required><br><br>
-  Apellidos: <input type=text name=apellidos required><br><br>
-  Cédula: <input type=text name=cedula required><br><br>
-  Fecha de Emisión: <input type=date name=fecha_emision required><br><br>
-  Cargo: <input type=text name=cargo required><br><br>
-  <input type=submit value=Agregar>
-</form>
-
-{% if qr_url %}
-  <p><strong>QR generado:</strong></p>
-  <img src="{{ qr_url }}">
-{% endif %}
-
-<h2>Lista de Personas</h2>
-<ul>
-{% for persona in personas %}
-  <li>{{ persona[1] }} {{ persona[2] }} - <a href="/persona/{{ persona[0] }}">Ver</a> | <a href="/editar/{{ persona[0] }}">Editar</a> | <a href="/eliminar/{{ persona[0] }}">Eliminar</a> | <a href="/pdf/{{ persona[0] }}">PDF</a></li>
-{% endfor %}
-</ul>
-'''
-
-profile_html = '''
-<!doctype html>
-<title>Perfil de Persona</title>
-<h2>Datos de la Persona</h2>
-<ul>
-  <li><strong>Nombres:</strong> {{ p[1] }}</li>
-  <li><strong>Apellidos:</strong> {{ p[2] }}</li>
-  <li><strong>Cédula:</strong> {{ p[3] }}</li>
-  <li><strong>Fecha de Emisión:</strong> {{ p[4] }}</li>
-  <li><strong>Cargo:</strong> {{ p[5] }}</li>
-</ul>
-<a href="/">Volver</a>
-'''
-
-edit_html = '''
-<!doctype html>
-<title>Editar Persona</title>
-<h2>Editar Persona</h2>
-<form method=post>
-  Nombres: <input type=text name=nombres value="{{ p[1] }}" required><br><br>
-  Apellidos: <input type=text name=apellidos value="{{ p[2] }}" required><br><br>
-  Cédula: <input type=text name=cedula value="{{ p[3] }}" required><br><br>
-  Fecha de Emisión: <input type=date name=fecha_emision value="{{ p[4] }}" required><br><br>
-  Cargo: <input type=text name=cargo value="{{ p[5] }}" required><br><br>
-  <input type=submit value=Guardar>
-</form>
-<a href="/">Volver</a>
-'''
 
 @app.route('/', methods=['GET', 'POST'])
 def agregar_persona():
@@ -104,7 +48,7 @@ def agregar_persona():
     personas = c.fetchall()
     conn.close()
 
-    return render_template_string(form_html, qr_url=qr_url, personas=personas)
+    return render_template('form.html', qr_url=qr_url, personas=personas)
 
 @app.route('/persona/<persona_id>')
 def ver_persona(persona_id):
@@ -115,7 +59,7 @@ def ver_persona(persona_id):
     conn.close()
 
     if p:
-        return render_template_string(profile_html, p=p)
+        return render_template('profile.html', p=p)
     else:
         return "Persona no encontrada", 404
 
@@ -138,7 +82,7 @@ def editar_persona(persona_id):
         c.execute("SELECT * FROM personas WHERE id=?", (persona_id,))
         p = c.fetchone()
         conn.close()
-        return render_template_string(edit_html, p=p)
+        return render_template('edit.html', p=p)
 
 @app.route('/eliminar/<persona_id>')
 def eliminar_persona(persona_id):
